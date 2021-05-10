3fa1d25b4f4755918527fc2d07cae56c82644860
Ronak Mehta <rmehta004@gmail.com>
2021-05-10 14:31:02 -0700

    Added timing, data downsampling, and increased memory requirement.

--- binarize.py
+++ binarize.py
@@ -1,5 +1,9 @@
+print("Importing dependencies...")
+
 import numpy as np
 from utils import KNeighborsClassifierCV
+import sys
+import numpy as np
 from sklearn.metrics import confusion_matrix
 import pickle
 
@@ -8,28 +12,63 @@ import pickle
 max_n_neighbors = 10
 n_splits = 5
 
+if len(sys.argv) > 1:
+    downsample_ratio = float(sys.argv[1])
+else:
+    downsample_ratio = 1
+
+print("Loading training data...")
+
 X_train = np.load("data/X_train.npy")
 y_train = np.load("data/y_train.npy")
-X_test = np.load("data/X_test.npy")
-y_test = np.load("data/y_test.npy")
+
+np.random.seed(123)
+np.random.shuffle(X_train)
+n = len(X_train)
+idx = np.arange(n)[0:int(n * downsample_ratio)]
+X_train = X_train[idx, :]
+y_train = y_train[idx]
+
+print("Subsetting training data to %d/%d points..." % (int(n * downsample_ratio), n))
 
 print(
     "Fitting adaptive k-neighest neighbors using %d-fold cross validation for k = 1, ..., %d..."
     % (n_splits, max_n_neighbors)
 )
 
-model = KNeighborsClassifierCV(max_n_neighbors=max_n_neighbors, n_splits=n_splits).fit(
+model = KNeighborsClassifierCV(max_n_neighbors=max_n_neighbors, n_splits=n_splits, verbose=True).fit(
     X_train, y_train
 )
+
+print("Loading test data...")
+
+X_test = np.load("data/X_test.npy")
+y_test = np.load("data/y_test.npy")
+
+if len(sys.argv) > 2:
+    downsample_ratio = float(sys.argv[2])
+else:
+    downsample_ratio = 1
+n = len(X_test)
+idx = np.arange(n)[0:int(n * downsample_ratio)]
+
+print("Subsetting test data to %d/%d points..." % (int(n * downsample_ratio), n))
+
+X_test = X_test[idx, :]
+y_test = y_test[idx]
+
+print("Computing evaluation metrics...")
 
 best_params = model.get_params()
 best_confusion_matrix = confusion_matrix(y_test, model.predict(X_test))
 
-pickle.dump(best_params, open("params.p", "rb"))
+pickle.dump(best_params, open("params.p", "wb"))
 with open("confusion_matrix.npy", "wb") as f:
     np.save(f, y_test)
 
 # TODO: Most confusing pair of classes.
 # TODO: binarizing data
 # TODO: print shape
+
+print("Done!")
 
--- utils.py
+++ utils.py
@@ -4,13 +4,26 @@ from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
 from sklearn.exceptions import NotFittedError
 from joblib import Parallel, delayed
+import time
+import datetime
+
+def format_time(elapsed):
+    """
+    Takes a time in seconds and returns a string hh:mm:ss
+    """
+    # Round to the nearest second.
+    elapsed_rounded = int(round((elapsed)))
+
+    # Format as hh:mm:ss
+    return str(datetime.timedelta(seconds=elapsed_rounded))
 
 
 class KNeighborsClassifierCV:
-    def __init__(self, max_n_neighbors=10, n_splits=5, score=accuracy_score):
+    def __init__(self, max_n_neighbors=10, n_splits=5, score=accuracy_score, verbose=False):
         self.max_n_neighbors = max_n_neighbors
         self.n_splits = n_splits
         self.score = score
+        self.verbose = verbose
 
     def fit(self, X, y):
 
@@ -21,13 +34,27 @@ class KNeighborsClassifierCV:
             X_train, X_val = X[train_index], X[val_index]
             y_train, y_val = y[train_index], y[val_index]
 
+            fold_tic = time.time()
+
             def evaluate(k):
+
+                tic = time.time()
                 model = KNeighborsClassifier(n_neighbors=k).fit(X_train, y_train)
+                toc = time.time()
+
+                if self.verbose:
+                    print("Fold %d/%d:  %d-nearest neighbors fit time: %s." % (i + 1, self.n_splits, k, format_time(toc-tic)))
+
                 return self.score(y_val, model.predict(X_val))
 
             scores[:, i] = Parallel(n_jobs=-2)(
                 delayed(evaluate)(k) for k in range(1, self.max_n_neighbors + 1)
             )
+
+            fold_toc = time.time()
+            
+            if self.verbose:
+                print("Fold %d/%d time: %s." % (i + 1, self.n_splits, format_time(fold_toc-fold_tic)))
 
         best_n_neighbors = np.argmax(scores.mean(axis=1))
         self.best_model = KNeighborsClassifier(n_neighbors=best_n_neighbors).fit(X, y)
